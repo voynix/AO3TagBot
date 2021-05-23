51bc1ac089d93141b9e07aca4d0db3a82c151aa7
voynix <voynix26@gmail.com>
2021-05-23 00:34:35 -0400

    follow links and extract tags from them
    Make a reasonable attempt to handle errors, but no guarantees this is fully robust

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 import logging
-from typing import List
+from typing import Dict, List, Optional
 
-from telegram import Update
+from bs4 import BeautifulSoup
+import requests
+from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
     CommandHandler,
@@ -12,11 +14,14 @@ from telegram.ext import (
 )
 
 
-# AO3 story URLS look like https://archiveofourown.org/works/WORK_ID/chapters/CHAPTER_ID
+# AO3 story URLS look like https://archiveofourown.org/works/WORK_ID[/chapters/CHAPTER_ID]
 AO3_STORY_URL_STARTS = [
     "https://archiveofourown.org/works",
     "archiveofourown.org/works",
 ]
+
+# seconds to wait for responses from AO3
+REQUEST_TIMEOUT = 15
 
 
 def normalize_url(url: str) -> str:
@@ -42,6 +47,43 @@ def find_ao3_story_urls(text: str) -> List[str]:
 
     logging.debug(f"Found AO3 links {links}")
     return links
+
+
+def get_tags_from_list(soup: BeautifulSoup, tag_list_class: str) -> Optional[str]:
+    tag_list_node = soup.find("dd", class_=tag_list_class)
+    if not tag_list_node:
+        return None
+    tags = []
+    for tag_node in tag_list_node.find_all("a"):
+        tags.append(tag_node.string)
+    return ", ".join(tags)
+
+
+def get_tags_for_story_url(url: str) -> Optional[Dict[str, str]]:
+    logging.debug(f"Retrieving and extracting tags from {url}")
+    response = requests.get(url, timeout=REQUEST_TIMEOUT)
+    if response.status_code != 200:
+        logging.debug(f"Got non-200 status code {response.status_code} for URL {url}")
+        return None
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # this is where the fun begins!
+    tag_info = {}
+    title_node = soup.find("h2", class_="title")
+    if title_node:
+        tag_info["title"] = title_node.string
+    tag_info["rating"] = get_tags_from_list(soup, "rating")
+    tag_info["warnings"] = get_tags_from_list(soup, "warning")
+    tag_info["categories"] = get_tags_from_list(soup, "category")
+    tag_info["fandoms"] = get_tags_from_list(soup, "fandom")
+    tag_info["relationships"] = get_tags_from_list(soup, "relationship")
+    tag_info["characters"] = get_tags_from_list(soup, "characters")
+    tag_info["tags"] = get_tags_from_list(soup, "freeform")
+
+    # filter tags we failed to find out of tag_info
+    tag_info = {k: v for k, v in tag_info.items() if v is not None}
+
+    return tag_info
 
 
 def start_command(update: Update, context: CallbackContext) -> None:
@@ -73,7 +115,40 @@ def message_reply(update: Update, context: CallbackContext) -> None:
     if urls:
         logging.debug(f"Found AO3 URLs {urls} in message")
         for url in urls:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=url)
+            # TODO: error handling
+            # TODO: wrap in a try/except that does something generic but sane
+            tag_info = get_tags_for_story_url(url)
+            logging.info(
+                f"Sending message to {update.effective_chat.full_name} for URL {url}"
+            )
+            if tag_info is None:
+                message_text = (
+                    f"Could not extract tags for {url}; does the story exist?"
+                )
+            else:
+                message_text = ""
+                if "title" in tag_info:
+                    message_text += f"\n<b>{tag_info['title']}</b>"
+                for key in [
+                    "rating",
+                    "warnings",
+                    "categories",
+                    "fandoms",
+                    "relationships",
+                    "characters",
+                    "tags",
+                ]:
+                    if key in tag_info:
+                        message_text += f"\n<b>{key.capitalize()}:</b> {tag_info[key]}"
+                if not message_text:
+                    message_text = (
+                        f"Could not extract tags for {url}; is the story locked?"
+                    )
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=message_text,
+                parse_mode=ParseMode.HTML,
+            )
 
 
 def main() -> None:
